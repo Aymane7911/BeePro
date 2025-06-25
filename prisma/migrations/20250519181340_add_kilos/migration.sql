840b41115bbb8ebdde6c6470e92b979604da49d1
Cherki Aymane <115834696+Aymane7911@users.noreply.github.com>
2025-06-25 10:46:16 +0400

    Delete prisma directory

--- prisma/migrations/20250519181340_add_kilos/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Apiary" ADD COLUMN     "kilosCollected" DOUBLE PRECISION NOT NULL DEFAULT 0;
--- prisma/migrations/20250523065440_report/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Batch" ADD COLUMN     "lab_report_path" TEXT,
-ADD COLUMN     "production_report_path" TEXT;
